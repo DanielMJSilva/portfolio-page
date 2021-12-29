6babd3bd6ccca22c7aa92995418493a0c97fcb71
DanielMJSilva <70338659+DanielMJSilva@users.noreply.github.com>
2021-12-28 19:45:27 -0500

    nav_bar added but needs style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,34 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+   // basic routes
+   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+   { path: 'welcome', component: WelcomeComponent },
+   { path: 'contactme', component: ContactMeComponent},
+   { path: 'portfolio', component: PortfolioComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    ContactMeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
